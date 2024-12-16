a35bd728ff38a14f6a1e56bea4d4b8890e51fb05
camila-vasconcelos-byx <camila.vasconcelos@grupobyx.com.br>
2024-12-16 08:34:10 -0300

    front - início

--- IMDbyx/views.py
+++ IMDbyx/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'IMDbyx/index.html')
+
+def movie_detail(request, movie):
+    return render(request, 'IMDbyx/movie.html')
