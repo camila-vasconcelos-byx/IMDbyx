47a3c40a77f8ae0529ba1d1a7aa698daf6d24be7
camila-vasconcelos-byx <camila.vasconcelos@grupobyx.com.br>
2024-12-23 15:21:42 -0300

    Redirecionar url vazio para a API

--- movie_project/urls.py
+++ movie_project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('IMDbyx.urls')),
-    path('user/', include('users.urls'))
+    path('user/', include('users.urls')),
+    path('', lambda request: redirect('api/', permanent=False))
 ]
